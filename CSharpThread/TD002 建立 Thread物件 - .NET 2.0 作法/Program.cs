862c59fb81c176cdae325873bfe467175cc3b340
Vulcan Lee <vulcan.lee@gmail.com>
2022-02-17 13:52:52 +0800

    可以不再需要使用 new 委派型別

--- CSharpThread/TD002 建立 Thread物件 - .NET 2.0 作法/Program.cs
+++ CSharpThread/TD002 建立 Thread物件 - .NET 2.0 作法/Program.cs
@@ -11,20 +11,25 @@ namespace TD002_建立_Thread物件___.NET_2._0_作法
     {
         static void Main(string[] args)
         {
-            Thread thread1 = new Thread(delegate ()
-            {
-                for (int i = 0; i < 500; i++)
-                {
-                    Console.Write("*");
-                }
-            });
-            Thread thread2 = new Thread(delegate (object message)
-            {
-                for (int i = 0; i < 500; i++)
-                {
-                    Console.Write(message.ToString());
-                }
-            });
+            //Thread thread1 = new Thread(delegate ()
+            //{
+            //    for (int i = 0; i < 500; i++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //});
+            //Thread thread2 = new Thread(delegate (object message)
+            //{
+            //    for (int i = 0; i < 500; i++)
+            //    {
+            //        Console.Write(message.ToString());
+            //    }
+            //});
+
+            #region .NET Framework 2.0 的非匿名委派用法
+            Thread thread1 = new Thread(M1);
+            Thread thread2 = new Thread(M2b);
+            #endregion
 
             Console.WriteLine("將要休息五秒鐘，" +
                 "觀察看看剛剛建立的兩個執行緒，是否有啟動");
@@ -41,5 +46,20 @@ namespace TD002_建立_Thread物件___.NET_2._0_作法
             Console.WriteLine("Press any key for continuing...");
             Console.ReadKey();
         }
+        private static void M1()
+        {
+            for (int i = 0; i < 500; i++)
+            {
+                Console.Write("*");
+            }
+        }
+        private static void M2(object message)
+        {
+            for (int i = 0; i < 500; i++)
+            {
+                Console.Write(message.ToString());
+            }
+        }
     }
+
 }
